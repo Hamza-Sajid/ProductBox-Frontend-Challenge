3fd837cb328f78dcd9516405f5d9a8178b998523
Hamza-Sajid <pak786hamzasajid66@gmail.com>
2025-03-07 09:52:49 +0500

    side bar menu design optimize

--- client/src/components/SideBarMenu/SideBarMenu.tsx
+++ client/src/components/SideBarMenu/SideBarMenu.tsx
@@ -5,16 +5,17 @@ import MenuButton from '../MenuButton/MenuButton'
 
 const SideBarMenu = () => {
   return (
-    <div className='flex flex-col gap-10  border-r-1 border-gray-300 h-screen'>
-      <div>
+    <div className='relative flex flex-col border-r-1 border-gray-300 items-center h-screen'>
+      <div className='h-[15%] w-full'>
         <img src={Logo} width={80} height={80}></img>
       </div>
-      {menuButtons.map((button, index) => {
-        return (
-          <MenuButton key={index} text={button.text} icon={button.icon} />
-        )
-      })}
-      <hr className="w-1/2 block m-auto rounded-3xl text-gray-300" />
+      <div className='flex flex-col gap-8 w-3/4 h-[30%] items-center justify-center'>
+        {menuButtons.map((button, index) => {
+          return (
+            <MenuButton key={index} text={button.text} icon={button.icon} path={button.path} />
+          )
+        })}
+      </div>
     </div>
   )
 }
